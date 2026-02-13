88d951496ad1643d50a5484744d7365a90d9c903
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-02-13 11:29:23 -0300

    Colhendo dados de registro

--- front-end/js/registro.js
+++ front-end/js/registro.js
@@ -5,8 +5,28 @@ function voltar() {
     } else {
         window.location.href = "../index.html";
     }
-
 }
 
 const botao = document.getElementById("seta")
 botao.addEventListener("click", voltar)
+
+function registrar() {
+    const user = document.getElementById("usuario")
+    const nome = user.value
+
+    const senha = document.getElementById("senha")
+    const code = senha.value
+
+    const isonepiece = document.getElementById("onepiece")
+    const faonepiece = isonepiece.checked
+
+    const isflamengo = document.getElementById("flamengo")
+    const torcefla = isflamengo.checked
+
+    const issousa = document.getElementById("sousa")
+    const sousa = issousa.checked
+
+}
+
+const registro = document.getElementById("registrar")
+registro.addEventListener("click", registrar)
