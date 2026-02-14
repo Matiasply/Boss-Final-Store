53dc8e83cac13db8f7f046e3fd6b9b80c4afe178
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-02-14 12:17:01 -0300

    Mensagem mostra em tempo real se usuário existe

--- front-end/js/registro.js
+++ front-end/js/registro.js
@@ -10,6 +10,30 @@ function voltar() {
 const botao = document.getElementById("seta")
 botao.addEventListener("click", voltar)
 
+// Função para validar nome de usuário em tempo real
+const inputNome = document.getElementById("usuario");
+const msg = document.getElementById("msg-user");
+
+//Assim que o usuário começar a digitar a função será chamada
+inputNome.addEventListener("input", async function() {
+    const nome = inputNome.value;
+
+    if (nome.length < 3) {
+        msg.textContent = "Digite pelo menos 3 caracteres";
+        msg.style.color = "black";
+        return;
+    }
+
+    // Faz requisição em tempo real para verificar o nome
+    const res = await fetch(`http://localhost:3000/verificar-nome/${nome}`);
+    const data = await res.json();
+
+    if (data.existe) {
+        msg.textContent = "❌ Nome já está em uso";
+        msg.style.color = "red";
+    }
+});
+
 // Função responsável por enviar os dados do usuário para o back
 async function registrar() {
     const user = document.getElementById("usuario")
@@ -44,14 +68,14 @@ async function registrar() {
             });
             
             const dados = await resposta.json();
-            console.log(dados)
+            console.log(dados.erro)
             
             
             /* Se o usuário for cadastrado, ele é
             redirecionado para a página principal*/
             if (dados.mensagem == "Usuário cadastrado") {
                 window.location.href = "home.html";
-            }
+            } 
 
         } catch(erro) {
             console.error(erro);
