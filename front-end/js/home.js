d1c5607ac8838c7cf3ac797c45bb5f516ad3e385
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-02-17 15:55:32 -0300

    Verificação de usuário para abrir o perfilv1

--- front-end/js/home.js
+++ front-end/js/home.js
@@ -14,9 +14,32 @@ function nextImage(){
     document.getElementById("radio" + count).checked = true;
 }
 
-// Lógica para os botões
-function perfil() {
-    window.location.href = "perfil.html";
+// Lógica para abrir a página de perfil
+async function perfil() {
+
+    try {
+        const resposta = await fetch("http://localhost:3000/perfil", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        );
+
+        const dados = await resposta.json();
+        console.log(dados);
+
+        if(dados.erro === "Sem login") {
+            window.location.href = "../index.html";
+            return;
+        } else{
+            window.location.href = "perfil.html";
+        }
+        
+    } catch(erro) {
+        console.error(erro);
+    }
 }
 const botao_perfil = document.getElementById("btn-perfil")
 botao_perfil.addEventListener("click", perfil)
