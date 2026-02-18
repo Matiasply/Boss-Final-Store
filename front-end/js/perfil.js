c905da1dfdbbc8b702ff3676302b38a04178a612
Matias Monteiro <matiasmonteiroaraujo@gmail.com>
2026-02-18 11:59:13 -0300

    Dados do usuário aparecem na tela

--- front-end/js/perfil.js
+++ front-end/js/perfil.js
@@ -24,17 +24,42 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         const dados = await resposta.json();
         
-        const nome = dados.nome;
-        const pirata = dados.onepiece;
-        const flamenguista = dados.flamenguista;
-        const sousense = dados.sousense;
+        console.log(dados)
+        const nome = dados.nome
+        const pirata = dados.onepiece
+        const flamenguista = dados.flamenguista
+        const sousense = dados.sousense
 
         const user = document.getElementById("Nome do usuário");
         user.textContent = nome;
 
+        //  Fã de One Piece
+        const dado1 = document.getElementById("One Piece");
+        if(pirata) {
+            dado1.textContent = "Membro dos Chapéus de Palha"
+        } else {
+            dado1.textContent = "Cria do Akainu"
+        }
+
+        // Flamenguista
+        const dado2 = document.getElementById("flamenguista");
+        if(flamenguista) {
+            dado2.textContent = "Flamenguista"
+        } else {
+            dado2.textContent = "Não torce pro flamengo" // Pensar em algo melhor
+        }
+
+        // Sousense
+        const dado3 = document.getElementById("sousense");
+        if(sousense) {
+            dado3.textContent = "Sousense";
+        } else {
+            dado3.textContent = "Deve torcer pro galo" // Ver se vai ficar legal mesmo
+        }
+
 } catch(erro) {
     console.error(erro);
-}
+    }
 }
 )
 
